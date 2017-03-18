eb137b993f2d692017067122a4a385c79cd87b82
Jared Luxenberg <jared@jaredlux.com>
2017-03-17 17:43:48 -0700

    build

---
+++ lib/cluster.d.ts
@@ -0,0 +1,49 @@
+/// <reference types="geojson" />
+/// <reference types="react" />
+/// <reference types="mapbox-gl" />
+import * as React from 'react';
+import * as MapboxGL from 'mapbox-gl';
+import { Props as MarkerProps } from './marker';
+import * as GeoJSON from 'geojson';
+export interface Props {
+    ClusterMarkerFactory(coordinates: GeoJSON.Position, pointCount: number): JSX.Element;
+    clusterThreshold?: number;
+    radius?: number;
+    maxZoom?: number;
+    minZoom?: number;
+    extent?: number;
+    nodeSize?: number;
+    log?: boolean;
+    children?: Array<React.Component<MarkerProps, void>>;
+}
+export interface State {
+    superC: any;
+    clusterPoints: any[];
+}
+export interface Context {
+    map: MapboxGL.Map;
+}
+export default class Cluster extends React.Component<Props, State> {
+    context: Context;
+    static contextTypes: {
+        map: React.Requireable<any>;
+    };
+    static defaultProps: {
+        clusterThreshold: number;
+        radius: number;
+        minZoom: number;
+        maxZoom: number;
+        extent: number;
+        nodeSize: number;
+        log: boolean;
+    };
+    state: {
+        superC: any;
+        clusterPoints: never[];
+    };
+    componentWillMount(): void;
+    private mapChange;
+    private feature(coordinates);
+    private childrenToFeatures;
+    render(): JSX.Element;
+}

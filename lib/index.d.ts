18c73c89680e663df0c105d0a62d2b7824fb224c
Jared Luxenberg <jared@jaredlux.com>
2017-04-02 21:08:31 -0700

    upgrade mapbox and build dist

---
+++ lib/index.d.ts
@@ -0,0 +1,12 @@
+import Map from './map';
+import Layer from './layer';
+import GeoJSONLayer from './geojson-layer';
+import Feature from './feature';
+import ZoomControl from './zoom-control';
+import Popup from './popup';
+import ScaleControl from './scale-control';
+import Marker from './marker';
+import Source from './source';
+import Cluster from './cluster';
+export { Feature, Layer, GeoJSONLayer, Map, Popup, ZoomControl, ScaleControl, Marker, Source, Cluster };
+export default Map;
